20c6a6ae2f9a21a1373ee4169ad853616b6c239f
DCODIE <djdimplejd@gmail.com>
2017-06-16 14:00:03 +0530

    .asm

--- add.asm
+++ add.asm
@@ -5,16 +5,16 @@ N1 DW 0009H
 N2 DW 0002H
 RES DW 4 DUP (0)
 .CODE
-START:    	MOV AX,@DATA
+START:    	          MOV AX,@DATA
           		MOV DS,AX
-   MOV AX,N1
+                              MOV AX,N1
           		MOV BX,N2
           		MOV DX,0000H
           		ADD AX,BX
           		JNC LAB1
           		INC DX
-LAB1: 	MOV RES,AX
+LAB1: 	                    MOV RES,AX
           		MOV RES+2,DX
-                        MOV AH,04CH
+                              MOV AH,04CH
           		INT 21H
           		END START
